3194588504c7dac4aad9636150d368aa9556623c
Andrey Maishev <fatym98@gmial.com>
2024-11-30 15:41:35 +0400

    14,2

--- src/Category.py
+++ src/Category.py
@@ -1,7 +1,10 @@
+from src.Product import Product
+
+
 class Category:
     name: str
     description: str
-    products: str
+    products: list
 
     category_count = 0
     product_count = 0
@@ -9,7 +12,24 @@ class Category:
     def __init__(self, name, desacription, products):
         self.name = name
         self.description = desacription
-        self.products = products
+        self.__products = products if products else []
 
         Category.category_count += 1
-        Category.product_count += len(products)
+        Category.product_count += len(self.__products)
+
+    @property
+    def products(self):
+        return self.__products
+
+    def add_product(self, new_product: Product):
+        self.products.append(new_product)
+        Category.product_count += 1
+
+    @property
+    def product_list(self):
+        product_str = ""
+        for product in self.products:
+            product_str += (
+                f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт.\n"
+            )
+        return product_str
--- tests/test_product.py
+++ tests/test_product.py
@@ -2,6 +2,14 @@ import pytest
 
 from src.Product import Product
 
+new_product = Product.new_product(
+    {
+        "name": "Samsung Galaxy S23 Ultra",
+        "description": "256GB, Серый цвет, 200MP камера",
+        "price": 180000.0,
+        "quantity": 5,
+    }
+)
 
 @pytest.fixture
 def product():
@@ -13,3 +21,10 @@ def test_init(product):
     assert product.description == "256GB, Серый цвет, 200MP камера"
     assert product.price == 180000.0
     assert product.quantity == 5
+
+
+def test_new_product():
+    new_product.price = 0
+    assert new_product.price == 180000
+    new_product.price = 800
+    assert new_product.price == 800
