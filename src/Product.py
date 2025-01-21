43fe43e2e9da5a0c14573196de32ce3ab2ae6af6
Andrey Maishev <fatym98@gmial.com>
2025-01-21 19:21:40 +0400

    16.1

--- src/Product.py
+++ src/Product.py
@@ -38,17 +38,19 @@ class Product:
         else:
             raise TypeError
 
+
 class Smartphone(Product):
     def __init__(self, name, desacription, price, quantity, efficiency, model, memory, color):
-        super().__init__(name,desacription, price, quantity)
+        super().__init__(name, desacription, price, quantity)
         self.efficiency = efficiency
         self.model = model
         self.memory = memory
         self.color = color
 
+
 class LawnGrass(Product):
     def __init__(self, name, desacription, price, quantity, country, germination_period, color):
-        super().__init__(name,desacription, price, quantity)
+        super().__init__(name, desacription, price, quantity)
         self.country = country
         self.germination_period = germination_period
         self.color = color
--- tests/test_product.py
+++ tests/test_product.py
@@ -1,6 +1,6 @@
 import pytest
 
-from src.Product import Product, Smartphone, LawnGrass
+from src.Product import LawnGrass, Product, Smartphone
 
 new_product = Product.new_product(
     {
@@ -11,29 +11,36 @@ new_product = Product.new_product(
     }
 )
 
+
 @pytest.fixture
 def product1():
     return Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
+
 
 @pytest.fixture
 def product2():
     return Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
 
+
 @pytest.fixture
 def smartphone1():
-    return Smartphone("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5, 95.5,"S23 Ultra", 256, "Серый")
+    return Smartphone("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5, 95.5, "S23 Ultra", 256, "Серый")
+
 
 @pytest.fixture
 def smartphone2():
     return Smartphone("Iphone 15", "512GB, Gray space", 210000.0, 8, 98.2, "15", 512, "Gray space")
 
+
 @pytest.fixture
 def grass1():
     return LawnGrass("Газонная трава", "Элитная трава для газона", 500.0, 20, "Россия", "7 дней", "Зеленый")
 
+
 @pytest.fixture
 def grass2():
     return LawnGrass("Газонная трава 2", "Выносливая трава", 450.0, 15, "США", "5 дней", "Темно-зеленый")
+
 
 def test_init(product1):
     assert product1.name == "Samsung Galaxy S23 Ultra"
@@ -48,8 +55,10 @@ def test_new_product():
     new_product.price = 800
     assert new_product.price == 800
 
+
 def test_sum_products(product1, product2):
     assert product1.price * product1.quantity + product2.price * product2.quantity == 2580000.0
+
 
 def test_smartphone(smartphone1):
     assert smartphone1.name == "Samsung Galaxy S23 Ultra"
